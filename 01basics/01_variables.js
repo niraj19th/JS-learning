1107eae81b33f19907e345bfd4925203a0a8086a
niraj19th <nirajkadel78@gmail.com>
2025-11-06 21:15:01 +0545

    datatypes learned
    datatypes

---
+++ 01basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 120
+let accountEmail = "nirajkadel78@gmail.com"
+var accountPassword = 19;
+ accountCity = "Kathmandu"
+ 
+ accountId = 2
+ console.log(accountId);
+ 
---
+++ 01basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all JS code as newer version
+
+// alert("3+2") // 
+
+console.group(3+3)// code readability should be high
+
+
+console.log("Niraj")
+
+let name= "Niraj"
+let age = 22
+let isloggedin = false
+let temperature = null
+
+// number => 2 to power 53
+//bigint => higher number
+//string => " "
+// boolean => true / false
+//null => standalone value
+// undefined= > not assigned value
+// symbol => unique
+
+//object =>
+
+
+console.log(typeof age);
